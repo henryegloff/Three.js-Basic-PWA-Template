3b06e9a30b61b8ca6891c5201050875ddfd9f9d1
Henry Egloff <henryegloff@users.noreply.github.com>
2023-07-21 14:41:13 +1000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -94,6 +94,16 @@ window.addEventListener('resize', () => {
 })
 
 
+// Register Service Worker
 
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function() {
+        navigator.serviceWorker.register('service-worker.js').then(function(registration) {
+            //console.log('ServiceWorker registration successful with scope: ', registration.scope);
+        }, function(err) {
+            //console.log('ServiceWorker registration failed: ', err);
+        })
+    })
+}
 
 
